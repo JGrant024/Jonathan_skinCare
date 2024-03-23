14f1c661709ee36af0125de4434bb60feacd0e61
JGrant024 <jonathan.grant23@icloud.com>
2024-03-23 07:30:23 -0400

    updated product cards with descriptions on the back

--- src/essential_products.js
+++ src/essential_products.js
@@ -11,36 +11,48 @@ export const ESSENTIAL_PRODUCTS = [
     productName: "Body Butter",
     price: "16.99",
     productImage: eproduct1,
+    description:
+      "Body butters are moisturizing and lightweight.                  INGREDIENTS: Shea Butter, Cocoa Butter, Coconut Oil, Almond Oil, Vitamin E",
   },
   {
     id: 2,
     productName: "Body Glow Oil",
     price: "17.99",
     productImage: eproduct2,
+    description:
+      "Ingredients: sweet almond oil, apricot kernel oil, vitamin c oil and body safe fragrance oil ",
   },
   {
     id: 3,
     productName: "Facial Glow Oil",
     price: "17.99",
     productImage: eproduct3,
+    description:
+      "INGREDIENTS: Argan Oil, Shea Nut Oil, Tea Tree Oil, Vitamin E. BENEFITS: Repairs skin and reduces fine lines and wrinkles.",
   },
   {
     id: 4,
     productName: "Glow Exfoliating Brush",
     price: "15.99",
     productImage: eproduct4,
+    description:
+      "Thanks to its rigid silicone surface, an electric silicone facial cleanser deeply cleanses pores to remove impurities, makeup and excess oil.",
   },
   {
     id: 5,
     productName: "Start Kit",
     price: "20.99",
     productImage: eproduct5,
+    description:
+      "With the choice of your favorite scent, this kit is perfect for our newbies! It includes a 4 oz Body Butter and 4 oz Body Oil.",
   },
   {
     id: 6,
     productName: "Terry Cloth Head Band",
     price: "9.99",
     productImage: eproduct6,
+    description:
+      "For those of you who struggle to wash your face after a long day of work, using a headband will make that process much easier by doing the work of keeping hair out of the equation.",
   },
 ];
 
