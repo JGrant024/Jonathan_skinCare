241bcf715724724d108b4f9fcea6139cb177e565
JGrant024 <jonathan.grant23@icloud.com>
2024-03-29 09:24:51 -0400

    completed updates to flip card, and about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
 const About = () => {
   return (
-    <>
-      <h1 style={{ color: "White" }}>About</h1>
-      <p> EST. JUNE 202</p>
-    </>
+    <div className="min-h-screen bg-gray-800 flex flex-col items-center justify-center text-white p-4">
+      <h1 className="text-4xl font-bold mb-4">About</h1>
+      <div className="max-w-2xl text-center">
+        <span className="text-lg font-semibold">EST. JUNE 2020</span>
+        <p className="mt-4">Welcome to ESSENTIAL'D SKIN!</p>
+        <p className="mt-4">
+          Essentiald Skincare was established in 2020. Our Mission is to create
+          natural skin products that heal and soothe the body. We are a vegan
+          and paraben-free company.
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import logo from "../assets/Images/Essentiald_logo.avif";
 import apply from "../assets/Images/apply.png";
 import Survey from "../assets/Images/Survey.png";
@@ -21,7 +20,8 @@ const products = [
   {
     name: "Whipped Butter",
     image: Whipped_butter,
-    description: "Rich and creamy whipped butter for your skin.",
+    description:
+      "Rich and creamy whipped butter for your skin. Whipped to perfection.",
   },
   {
     name: "Skin Investment",
@@ -31,20 +31,22 @@ const products = [
   {
     name: "Boxed Oil",
     image: Boxed_oil,
-    description: "Essential oils for daily skincare routine.",
+    description:
+      "Essential oils for daily skincare routine. Try some of our various scents!",
   },
   {
     name: "Surveys Are In",
     image: Survey,
-    description: "Your feedback matters. See what others are saying!",
+    description:
+      "Your feedback matters. See what others are saying about our natural body butters!",
   },
 ];
 
 function Home() {
   return (
-    <div className="font-sans bg-yellow-100 p-10">
+    <div className="font-sans bg-yellow-100 p-10 padding-custom">
       <div className="text-center mb-10 bg-amber-900 text-white py-6">
-        <h1 className="text-4xl font-bold">Welcome to Essential'd Skincare</h1>
+        <h1 className="text-4xl font-bold">Welcome to Essential'd Skin Care</h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product, index) => (
